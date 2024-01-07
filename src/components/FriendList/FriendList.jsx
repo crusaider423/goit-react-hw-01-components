891b5feba8839fb204306dd15b95894e79401b5f
Олександр Канюка <137859612+crusaider423@users.noreply.github.com>
2024-01-07 17:02:16 +0200

    update

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,19 +1,16 @@
 import css from './friendList.module.css';
-
+import { FriendListItem } from 'components/FriendListItem/FriendListItem';
 export const FriendList = ({ friends }) => {
   return (
     <ul className={css.list}>
-      {friends.map(({ avatar, name, isOnline, id }) => {
-        return (
-          <li className={css.item} key={id}>
-            <span
-              className={`${css.status} ${isOnline ? css.online : css.offline}`}
-            ></span>
-            <img className={css.avatar} src={avatar} alt={name} width="48" />
-            <p className={css.name}>{name}</p>
-          </li>
-        );
-      })}
+      {friends.map(({ avatar, name, isOnline, id }) => (
+        <FriendListItem
+          key={id}
+          avatar={avatar}
+          name={name}
+          isOnline={isOnline}
+        />
+      ))}
     </ul>
   );
 };
---
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -0,0 +1,13 @@
+import css from '../FriendList/friendList.module.css';
+
+export const FriendListItem = ({ avatar, name, isOnline }) => {
+  return (
+    <li className={css.item}>
+      <span
+        className={`${css.status} ${isOnline ? css.online : css.offline}`}
+      ></span>
+      <img className={css.avatar} src={avatar} alt={name} width="48" />
+      <p className={css.name}>{name}</p>
+    </li>
+  );
+};
